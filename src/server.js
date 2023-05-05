e8061ae35a65c2b2bbcd7b84b05856dea8f7ac04
ManuelForneri <manuelforneri@hotmail.com>
2023-05-05 12:29:23 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,14 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({ msg: "Hello World!" });
+});
+app.get("/products", (req, res) => {
+  res.json({
+    name: "Producto 1",
+    price: 50,
+    description: "dmidjiejdjejdieidiejijded",
+  });
 });
 
 app.listen(port, () => {
