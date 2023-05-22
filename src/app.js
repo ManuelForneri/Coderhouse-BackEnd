6fbc8892e0697d10f7e23903f683110215e9ad7d
manuel forneri <manuelforneri@hotmail.com>
2023-05-22 19:46:17 -0300

    update clase 9 (47:00)

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
+//@ts-check
 import express from "express";
 import { productsRouter } from "./routes/products.routes.js";
 import { cartRouter } from "./routes/cart.routes.js";
 import { testPlantillaProducts } from "./routes/test-plantilla-products.routes.js";
+import handlebars from "express-handlebars";
+import { __dirname } from "./utils.js";
+import path from "path";
 
 const app = express();
 const port = 8080;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Bienvenidos");
@@ -16,6 +22,10 @@ app.get("/", (req, res) => {
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartRouter);
 app.use("/test-plantilla-products", testPlantillaProducts);
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", path.join(__dirname, "views "));
+app.set("view engine", "handlebars");
 
 app.get("*", (req, res) => {});
 app.listen(port, () => {
--- src/routes/test-plantilla-products.routes.js
+++ src/routes/test-plantilla-products.routes.js
@@ -6,18 +6,21 @@ export const testPlantillaProducts = Router();
 import { ProductManager } from "../ProductManager.js";
 const ProductM = new ProductManager();
 
+// testPlantillaProducts.get("/", (req, res) => {
+//   let products = ProductM.getProducts();
+//   const query = req.query;
+//   console.log(query);
+//   if (!!query.limit) {
+//     let limit = parseInt(query.limit);
+//     let prodLimits = [];
+//     for (let i = 0; i < limit; i++) {
+//       prodLimits.push(products[i]);
+//     }
+//     res.json(prodLimits);
+//   } else {
+//     res.json(products);
+//   }
+// });
 testPlantillaProducts.get("/", (req, res) => {
-  let products = ProductM.getProducts();
-  const query = req.query;
-  console.log(query);
-  if (!!query.limit) {
-    let limit = parseInt(query.limit);
-    let prodLimits = [];
-    for (let i = 0; i < limit; i++) {
-      prodLimits.push(products[i]);
-    }
-    res.json(prodLimits);
-  } else {
-    res.json(products);
-  }
+  return res.status(200).render("test-plantilla-products", {});
 });
