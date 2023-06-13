43c353e74ed317432605dabf9572e4d3f80fe169
ManuelForneri <manuelforneri@hotmail.com>
2023-06-13 18:42:45 -0300

    se actualizo home

--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -1,15 +1,32 @@
 import express from "express";
-import { PServives } from "../services/products.service.js";
+import { ProductManager } from "../DAO/ProductManager.js";
+const ProductM = new ProductManager();
+//import { PServives } from "../services/products.service.js";
 
 export const home = express.Router();
 
 home.get("/", async (req, res) => {
+  const title = "Listado de productos";
+  let products = ProductM.getProducts();
+  const query = req.query;
+
+  if (!!query.limit) {
+    let limit = parseInt(query.limit);
+    let prodLimits = [];
+    for (let i = 0; i < limit; i++) {
+      prodLimits.push(products[i]);
+      return res.status(200).render("home", { title, prodLimits });
+    }
+  } else {
+    return res.status(200).render("home", { title, products });
+  }
+
+  /*quise implementar la base de datos aca pero no funciono
   try {
-    const title = "Listado de productos";
     const query = req.query;
     if (!!query.limit) {
       const products = await PServives.getLimit(query.limit);
-      return res.status(200).render("home", { title, products });
+      return res.status(200).render("home", title, products);
     } else {
       const products = await PServives.getAll();
       console.log(products);
@@ -23,4 +40,5 @@ home.get("/", async (req, res) => {
       payload: {},
     });
   }
+  */
 });
