70f89076d9ed36c5febdc4b604d4042533c7ea4a
ManuelForneri <manuelforneri@hotmail.com>
2023-06-28 13:43:32 -0300

    clase 18 1hs 39 min

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-//@ts-check
 import express from "express";
 import handlebars from "express-handlebars";
 import { __dirname } from "./config.js";
@@ -11,13 +10,19 @@ import { usersHtmlRouter } from "./routes/users.html.routes.js";
 import { productsRouter } from "./routes/products.routes.js";
 import { connectMongo } from "./utils/dbConnection.js";
 import { connectSocketServer } from "./utils/socketServer.js";
-import { cookiesRouter } from "./routes/cookies.routes.js";
-import { sessionsRouter } from "./routes/sessions.routes.js";
+//import { cookiesRouter } from "./routes/cookies.routes.js";
+//import { sessionsRouter } from "./routes/sessions.routes.js";
+//import cookieParser from "cookie-parser";
+import session from "express-session";
 
 const app = express();
 const port = 8080;
 
 connectMongo();
+//app.use(cookieParser());
+app.use(
+  session({ secret: "un-re-secreto", resave: true, saveUninitialized: true })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -33,37 +38,40 @@ const httpServer = app.listen(port, () => {
 });
 connectSocketServer(httpServer);
 
-app.use("/api/products", authenticate, productsRouter);
-app.use("/api/carts", authenticate, cartRouter);
-app.use("/api/users", authenticate, usersRouter);
+app.use("/api/products", /*authenticate,*/ productsRouter);
+app.use("/api/carts", /*authenticate,*/ cartRouter);
+app.use("/api/users", /*authenticate,*/ usersRouter);
 app.use("/html/users", usersHtmlRouter);
-app.use("/realtimeproducts", authenticate, realTimeProducts);
+app.use("/realtimeproducts", /*authenticate,*/ realTimeProducts);
 app.use("/chat", realTimeChat);
-app.use("/cookie", cookiesRouter);
-app.use("/api/sessions/", sessionsRouter);
-app.use("/", sessionsRouter);
-/*prueba cookies
-app.get("/set-cookies", (req, res) => {
-  res.cookie("cookie-test", "informacion valiosa", { maxAge: 100000 });
-  return res.json({
-    status: "Cookies",
-    msg: "cookies inyectados",
-    data: {},
-  });
-});
-*/
-function authenticate(req, res, next) {
-  if (!req.session.user) {
-    return res.render("errorLogin", { msg: "Error authenticate" });
+//app.use("/cookie", cookiesRouter);
+//app.use("/api/sessions/", sessionsRouter);
+//app.use("/", sessionsRouter);
+
+// function authenticate(req, res, next) {
+//   if (!req.session.user) {
+//     return res.render("errorLogin", { msg: "Error authenticate" });
+//   }
+//   next();
+// }
+
+app.get("/session", (req, res) => {
+  if (req.session.cont) {
+    req.session.cont++;
+    res.send("nos visitaste " + req.session.cont);
+  } else {
+    req.session.cont = 1;
+    req.session.name = "manuel";
+    req.session.lastSearch = "hyperx-cloud-fligth";
+
+    res.send("nos visitaste " + 1);
   }
-  next();
-}
-
-app.get("*", (req, res) => {
-  return res.render("errorLogin", { msg: "Error link" });
 });
-
 app.use("/", home);
+// app.get("*", (req, res) => {
+//   return res.render("errorLogin", { msg: "Error link" });
+// });
+
 app.get("*", (req, res) => {
   return res.status(404).send("not found");
 });
