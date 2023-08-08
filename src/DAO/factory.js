8edc0a36a06a4e5d9cff5ca6868a63b5b7079fef
manuel forneri <manuelforneri@hotmail.com>
2023-08-08 19:47:58 -0300

    Update factory.js

--- src/DAO/factory.js
+++ src/DAO/factory.js
@@ -4,37 +4,37 @@ import { productModel } from "./models/products.model.js";
 import { userModel } from "./models/users.model.js";
 import { cartsModel } from "./models/carts.model.js";
 
-
 async function importModels() {
-	let models;
+  let models;
 
-	switch (env.persistence) {
-		case "MONGO":
-			
-			mongoose.connect(env.mongoUrl);
-			models = {
-				products: productModel,
-				users: userModel,
-				carts: cartsModel,
-				tickets: ,
-			};
-			break;
+  switch (env.persistence) {
+    case "MONGO":
+      mongoose.connect(env.mongoUrl);
+      models = {
+        products: productModel,
+        users: userModel,
+        carts: cartsModel,
+        tickets: ticketsModel,
+      };
+      break;
 
-		case "MEMORY":
-			console.log("Database: Persistencia en memoria");
-			models = {
-				products: productsMemory,
-				users: usersMemory,
-				carts: cartsMemory,
-				tickets: ticketsMemory,
-			};
-			break;
+    case "MEMORY":
+      console.log("Database: Persistencia en memoria");
+      models = {
+        products: productsMemory,
+        users: usersMemory,
+        carts: cartsMemory,
+        tickets: ticketsMemory,
+      };
+      break;
 
-		default:
-			throw new Error(`El tipo de persistencia "${env.persistence}" no es válido.`);
-	}
+    default:
+      throw new Error(
+        `El tipo de persistencia "${env.persistence}" no es válido.`
+      );
+  }
 
-	return models;
+  return models;
 }
 
 export default importModels;
