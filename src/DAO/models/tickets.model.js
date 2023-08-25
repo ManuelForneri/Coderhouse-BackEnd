419c738dc1796cdf4bb86e30406a103a7ac19fdf
manuel forneri <manuelforneri@hotmail.com>
2023-08-24 21:37:25 -0300

    Mejorando la arquitectura del servidor v 3.0

--- src/DAO/models/tickets.model.js
+++ src/DAO/models/tickets.model.js
@@ -1,6 +1,7 @@
 import { cartsModel } from "./carts.model.js";
 import { ticketsMongoose } from "./mongoose/tickets.mongoose.js";
 import { productModel } from "./products.model.js";
+import { format } from "date-fns";
 
 class TicketModel {
   async getTicketById(tid) {
@@ -19,27 +20,47 @@ class TicketModel {
     //corroborar stock (model de producto)
     let cartFilter = [];
     let cartFilterOutStock = [];
+    let amount = 0;
 
     for (let i = 0; i < plainCart.length; i++) {
       if (plainCart[i].product.stock < plainCart[i].quantity) {
         cartFilterOutStock.push(plainCart[i]);
       } else {
         cartFilter.push(plainCart[i]);
-
+        amount += plainCart[i].product.price * plainCart[i].quantity;
         productModel.updateProduct({
           id: plainCart[i].product._id,
           stock: plainCart[i].product.stock - plainCart[i].quantity,
         });
       }
     }
-    //generar el ticket (usar dto al finalizar)
 
+    //generar el ticket (usar dto al finalizar)
+    let code = Date.now().toString();
+    const datePurchase = new Date();
+    const purchase_datetime = format(datePurchase, "dd/MM/yyyy HH:mm:ss");
+    const email = user.email;
+
+    /*no se si va DTO
+    const newTicket = new TicketsDTO({
+      cid,
+      code,
+      purchase_datetime,
+      amount,
+      email,
+      products: plainCart,
+    });
+    */
     // cuando se genera la compra, filtrar entre los que se compraron y los que no
     //en caso de que no hay stock devolver los productos que no se pudieron comprar
+    const ticketCreated = await ticketsMongoose.create({
+      code,
+      purchase_datetime,
+      amount,
+      purchaser: email,
+    });
 
-    // const ticketCreated = await ticketsMongoose.create(ticketCreated);
-    // return ticketCreated;
-    return plainCart;
+    return ticketCreated;
   }
   async verifyCart(cid, user) {
     const cart = await cartsModel.getCartById(cid);
--- src/controllers/DTO/tickets.DTO.js
+++ src/controllers/DTO/tickets.DTO.js
@@ -1,10 +1,10 @@
 export default class TicketsDTO {
   constructor(ticket) {
-    this.cartId = ticket.cart_id;
+    this.cartId = ticket.cid;
     this.code = ticket.code;
     this.purchase_datetime = ticket.purchase_datetime;
-    this.amount = ticket.total;
-    this.purchaser = ticket.usuario;
+    this.amount = ticket.amount;
+    this.purchaser = ticket.email;
     this.products = ticket.products;
   }
 }
